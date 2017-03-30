1e0c7ad50ac20ce9724a45724c1e07a1ddc6efce
D3ftsu <sahraouisharp@hotmail.com>
2017-03-30 20:28:57 +0100

    Fixed Q Prediction

--- KalistaPlusPlus/KalistaPlusPlus/SpellManager.cpp
+++ KalistaPlusPlus/KalistaPlusPlus/SpellManager.cpp
@@ -7,12 +7,12 @@ ISpell2* SpellManager::R;
 
 void SpellManager::Initialize()
 {
-	Q = GPluginSDK->CreateSpell2(eSpellSlot::kSlotQ, eSpellType::kLineCast, true, false, kCollidesWithYasuoWall);
+	Q = GPluginSDK->CreateSpell2(eSpellSlot::kSlotQ, eSpellType::kLineCast, true, false, eCollisionFlags::kCollidesWithMinions | eCollisionFlags::kCollidesWithYasuoWall);
 	Q->SetSkillshot(0.25f, 40.f, 2400.f, 1200.f);
-	W = GPluginSDK->CreateSpell2(eSpellSlot::kSlotW, eSpellType::kLineCast, false, false, kCollidesWithNothing);
+	W = GPluginSDK->CreateSpell2(eSpellSlot::kSlotW, eSpellType::kLineCast, false, false, eCollisionFlags::kCollidesWithNothing);
 	W->SetOverrideRange(5200.f);
-	E = GPluginSDK->CreateSpell2(eSpellSlot::kSlotE, eSpellType::kTargetCast, false, false, kCollidesWithNothing);
+	E = GPluginSDK->CreateSpell2(eSpellSlot::kSlotE, eSpellType::kTargetCast, false, false, eCollisionFlags::kCollidesWithNothing);
 	E->SetOverrideRange(1000.f);
-	R = GPluginSDK->CreateSpell2(eSpellSlot::kSlotR, eSpellType::kTargetCast, false, false, kCollidesWithNothing);
+	R = GPluginSDK->CreateSpell2(eSpellSlot::kSlotR, eSpellType::kTargetCast, false, false, eCollisionFlags::kCollidesWithNothing);
 	R->SetOverrideRange(1400.f);
 }
